852630c691ccb9ad0e673d50302993df8aa0e8ff
Karan Mittal <Karanmittal662@gmail.com>
2024-12-24 09:16:24 +0530

    Folder deleted

--- FormBackEnd/src/db/conn.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require("mongoose");
-mongoose.connect("mongodb://localhost:27017/customerRegistration",{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-}).then(()=>{
-    console.log(`Connection sucessfull`);
-}).catch((e)=>{
-    console.log(`Connection Error`,e);  
-}) 
